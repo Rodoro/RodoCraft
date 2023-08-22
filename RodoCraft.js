2c30c9b50b9a03f25ab5562f08cab6eb9e861297
Rodoro <rodoro.game@gmail.com>
2023-08-22 11:36:10 +0300

    Add lvl vip status config

--- RodoCraft.js
+++ RodoCraft.js
@@ -3,6 +3,7 @@ const { connect } = require('mongoose');
 const app = express();
 const port = 3000;
 const path = require('path')
+const fs = require('fs');
 const bodyParser = require("body-parser");
 const VipStatus = require('./models/vipstatus.js');
 const databaseToken = "mongodb+srv://Raphael:As213411@rodoro.wzfcq.mongodb.net/RodoCraft?retryWrites=true&w=majority";
@@ -15,7 +16,7 @@ db = true;
     await connect(databaseToken, { useNewUrlParser: true, useUnifiedTopology: true }).catch(console.error, db = false);
 })();
 
-//discordBot.botStart();
+discordBot.botStart();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,9 +42,12 @@ app.get('/admin/direction/vipstatus/', async (req, res) => {
 
 app.post('/admin/direction/vipstatus/', async (req, res) => {
   try {
-    const users = await VipStatus.find();
+    const users = null;
+    if(db==true){
+      users = await VipStatus.find();
+    }
 
-    res.render("vipstatus", {users});
+    res.render("vipstatus", {users, db});
   } catch (error) {
     console.error(error);
     res.status(500).send('Произошла ошибка сервера');
@@ -125,6 +129,51 @@ app.post('/admin/direction/vipstatus/save_add', async (req, res) => {
 app.get('/admin/direction/vipstatus/config', (req, res) => {
   const configData = require('./public/json/lvlVipStatus.json');
   res.json(configData);
+})
+
+app.get('/admin/direction/vipstatus/setting', (req, res) => {
+  const configData = require('./public/json/lvlVipStatus.json');
+  res.render('vipstatus_setting', {configData});
+})
+
+app.post('/admin/direction/vipstatus/setting/save', (req, res) => {
+
+  fs.readFile('public/json/lvlVipStatus.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Ошибка сервера');
+    }
+
+    try {
+      const json = JSON.parse(data);
+
+      json.discord.idRole = req.body.idRole;
+      json.discord.idRoleNews = req.body.idRoleNews;
+      json.minecraft.prefics = req.body.prefics;
+      json.minecraft.idCreativeServer = req.body.idCreativeServer;
+      json.craftToken.customItem = req.body.customItem;
+      json.craftToken.evredayGive = req.body.evredayGive;
+
+      var lvlBorders = [];
+      for (const key in req.body) {
+        if (key.startsWith('lvlBorder')) {
+          lvlBorders.push(req.body[key]);
+        }
+      }
+      json.lvlBorder = lvlBorders;
+
+      fs.writeFile('public/json/lvlVipStatus.json', JSON.stringify(json, null, 3), 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Ошибка сервера');
+        }
+  res.redirect('/admin/direction/vipstatus/');
+      })}
+    catch{
+      console.error(err);
+      return res.status(500).next;
+    }
+  })
 })
 
 app.use((req, res) => {
